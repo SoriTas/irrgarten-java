d2f85a239e3a1fff521e9eedf68285859569e457
manulg48 <lopezmanuel@correo.ugr.es>
2023-10-09 16:59:15 +0200

    Creacion clase player

---
+++ src/irrgarten/Player.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package irrgarten;
+
+/**
+ *
+ * @author manuel
+ */
+public class Player {
+    private static final int MAX_WEAPONS = 2;
+    private static final int MAX_SHIELDS = 2;
+    private static final int INITIAL_HEALTH
+}
