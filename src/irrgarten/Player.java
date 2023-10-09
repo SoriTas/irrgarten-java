0340f27af99c911897bdd0b54152b24d2c49c55a
manulg48 <lopezmanuel@correo.ugr.es>
2023-10-09 17:03:52 +0200

    Declaracion atributos player

--- src/irrgarten/Player.java
+++ src/irrgarten/Player.java
@@ -11,5 +11,14 @@ package irrgarten;
 public class Player {
     private static final int MAX_WEAPONS = 2;
     private static final int MAX_SHIELDS = 2;
-    private static final int INITIAL_HEALTH
+    private static final int INITIAL_HEALTH = 10;
+    private static final int HITS2LOSE = 3;
+    private String name;
+    private char number;
+    private float intelligence;
+    private float strength;
+    private float health;
+    private int row;
+    private int col;
+    private int consecutivesHits = 0;
 }
