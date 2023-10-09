44da1633e50b6e87447af0b33be7842f25c71123
sori <sori@pablubuntu>
2023-10-09 16:48:33 +0200

    Declaracion clase monster

--- src/irrgarten/TestP1.java
+++ src/irrgarten/TestP1.java
@@ -1,26 +1,16 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package irrgarten;
 
 /**
  *
- * @author manuel
+ * @author sori
  */
-enum Directions { LEFT, RIGHT, UP, DOWN}
-
-enum Orientation {VERTICAL, HORIZONTAL}
-
-enum GameCharacter {PLAYER, MONSTER}
-
-public class Irrgarten {
+public class TestP1 {
     
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static void main(){
         
         Weapon arma1 = new Weapon ((float) 1.5,5);
         Weapon arma2 = new Weapon ((float) 2.5, 3);
@@ -34,27 +24,27 @@ public class Irrgarten {
             System.out.println(arma2.toString());
             
             System.out.println(arma1.attack());
-            System.out.println(arma2.attack());
+            arma2.attack();
             
-            System.out.println(escudo1.toString());
-            System.out.println(escudo2.toString());
+            escudo1.toString();
+            escudo2.toString();
             
-            System.out.println(escudo1.protect());
-            System.out.println(escudo2.protect());
+            escudo1.protect();
+            escudo2.protect();
             
             System.out.println("Desechar armas:\n");
             
-            System.out.println(arma1.discard());
-            System.out.println(arma2.discard());
+            arma1.discard();
+            arma2.discard();
             
             System.out.println("Desechar escudos:\n");
             
-            System.out.println(escudo1.discard());
-            System.out.println(escudo2.discard());
+            escudo1.discard();
+            escudo2.discard();
             
         }
         
-        for(int i = 0; i < 100; i++){
+       /* for(int i = 0; i < 100; i++){
             
             Dice.discardElement(i+1);
             
@@ -85,9 +75,6 @@ public class Irrgarten {
             
         }
 
-           
-        
+            */
     }
-    
-    
 }
