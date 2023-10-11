5540364dbcbea8fc656347f21082770071972e14
manulg48 <lopezmanuel@correo.ugr.es>
2023-10-11 12:35:12 +0200

    declaracion de los metodos game

--- src/irrgarten/Game.java
+++ src/irrgarten/Game.java
@@ -13,5 +13,55 @@ public class Game {
     private int currentPlayerIndex;
     private String log;
     
-    
-}
+    public Game(int nPlayers){
+        throw new UnsupportedOperationException();
+    }
+    public boolean finished(){
+        throw new UnsupportedOperationException();
+    }
+    public boolean nextStep(Directions preferredDirection){
+        throw new UnsupportedOperationException();
+    }
+    public GameState getGameState(){
+        throw new UnsupportedOperationException();
+    }
+    private void configureLabyrinth(){
+        throw new UnsupportedOperationException();
+    }
+    private void nextPlayer(){
+        throw new UnsupportedOperationException();
+    }
+    private Directions actualDirection(Directions preferredDirection){
+        throw new UnsupportedOperationException();
+    }
+    private GameCharacter combat(Monster monster){
+        throw new UnsupportedOperationException();
+    }
+    private void manageReward(GameCharacter winner){
+        throw new UnsupportedOperationException();
+    }
+    private void manageResurrection(){
+        throw new UnsupportedOperationException();
+    }
+    private void logPlayerWon(){
+        throw new UnsupportedOperationException();
+    }
+    private void logMonsterWon(){
+        throw new UnsupportedOperationException();
+    }
+    private void logResurrected(){
+        throw new UnsupportedOperationException();
+    }
+    private void logPlayerSkipTurn(){
+        throw new UnsupportedOperationException();
+    }
+    private void logPlayerNoOrders(){
+        throw new UnsupportedOperationException();
+    }
+    private void logNoMonster(){
+        throw new UnsupportedOperationException();
+    }
+    private void logRounds(int rounds, int max){
+        throw new UnsupportedOperationException();
+    }
+}   
